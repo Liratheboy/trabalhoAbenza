8a4b86cb37542d6e130234f504906722cfcf8aa5
João Paulo Lira <joao.plbsilva@gmail.com>
2023-06-12 11:25:45 -0300

    muita coisa

--- script/script.js
+++ script/script.js
@@ -1,3 +1,101 @@
-let teste = document.getElementById('teste')
+const questao = document.getElementById("questao");
+const escolhas = Array.from(document.getElementsByClassName("texto-escolha"));
 
-teste.innerText = "testando os links"
+let questaoAtual = {};
+let aceitandoQuestoes = true;
+let pontuacao = 0;
+let contadorQuestao = 0;
+let questoesRestantes = [];
+
+let questoes = [
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  },
+  {
+    questao: "",
+    escolha1: "",
+    escolha2: "",
+    escolha3: "",
+    escolha4: "",
+    escolha5: "",
+    resposta: 1
+  }
+];
