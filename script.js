4760ccb518f1e8aaed27df9a8deb3f16dadd768b
AmiltonOsousa <119018582+AmiltonOsousa@users.noreply.github.com>
2023-06-18 14:24:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+let questaoAtual = 0;
+let acertos = 0;
+
+let respostaCerta = [4, 1, 5, 3, 2, 2, 4, 5, 3, 2];
+let pergunta = [];
+let escolhidas = [];
+
+pergunta[0] = "1) Para utilizamos a função srand(), precisamos incluir a biblioteca:";
+pergunta[1] = "2) Resumidamente, o que é uma função?";
+pergunta[2] = "3) Quantos espaços a seguinte matriz apresenta: int matriz[10][10]; ?";
+pergunta[3] = "4) Como podemos selecionar elementos por meio de classes e id's respectivamente?";
+pergunta[4] = "5) HTML é uma linguagem de ... ?";
+pergunta[5] = "6) \"For\" é uma estrutura de repetição controlada por ?";
+pergunta[6] = "7) No javascript, o que a função recebe entre os parênteses?";
+pergunta[7] = "8) CSS é uma linguagem de ...";
+pergunta[8] = "9) \"Const\" tem esse nome porque o valor atribuído é...";
+pergunta[9] = "10) O que o método \"appendChild\" faz?";
+
+
+let altA = [];
+
+altA[0] = "A) include time.h";
+altA[1] = "A) Um bloco de código que executa algo";
+altA[2] = "A) 10";
+altA[3] = "A) \"#\" e \".\"";
+altA[4] = "A) Estilização";
+altA[5] = "A) Variável";
+altA[6] = "A) Objetos";
+altA[7] = "A) Marcação";
+altA[8] = "A) Nulo";
+altA[9] = "A) Remove o nó especifico";
+
+
+let altB = [];
+
+altB[0] = "B) include <math.h>";
+altB[1] = "B) Uma biblioteca";
+altB[2] = "B) 0";
+altB[3] = "B) \"#\" e \"*\"";
+altB[4] = "B) Marcação";
+altB[5] = "B) Contador";
+altB[6] = "B) Paramêtros";
+altB[7] = "B) Programação";
+altB[8] = "B) Variável";
+altB[9] = "B) Adiciona um nó ao final da lista de filhos de um nó pai especificado.";
+
+
+let altC = [];
+
+altC[0] = "C) include <string.h>";
+altC[1] = "C) Uma linguagem de programação";
+altC[2] = "C) 1000";
+altC[3] = "C) \".\" e \"#\"";
+altC[4] = "C) Programação";
+altC[5] = "C) Indexador";
+altC[6] = "C) Classes";
+altC[7] = "C) Amor";
+altC[8] = "C) Constante";
+altC[9] = "C) Cria uma função";
+
+
+let altD = [];
+
+altD[0] = "D) include <time.h>";
+altD[1] = "D) Um laço de repetição";
+altD[2] = "D) 1";
+altD[3] = "D) \"%\" e \"!\"";
+altD[4] = "D) Banco de Dados";
+altD[5] = "D) Funções";
+altD[6] = "D) Argumentos";
+altD[7] = "D) Banco de Dados";
+altD[8] = "D) Zero";
+altD[9] = "D) Adiciona uma repetição";
+
+
+let altE = [];
+
+altE[0] = "E) include <ctype.h>";
+altE[1] = "E) Um tipo de variável";
+altE[2] = "E) 100";
+altE[3] = "E) \"+\" e \"=\"";
+altE[4] = "E) Sinais";
+altE[5] = "E) Objetos";
+altE[6] = "E) Nada";
+altE[7] = "E) Estilização";
+altE[8] = "E) Nenhuma das alternativas";
+altE[9] = "E) Nenhuma das alternativas";
+
+function next() {
+
+    if(questaoAtual == 10){
+        acertos = verAcertos();
+    }
+
+    passar.innerText = "Próxima pergunta";
+
+    alt1.disabled = false;
+    alt2.disabled = false;
+    alt3.disabled = false;
+    alt4.disabled = false;
+    alt5.disabled = false;    
+
+    questao.innerText = pergunta[questaoAtual];
+    
+    alt1.innerText = altA[questaoAtual];
+    alt2.innerText = altB[questaoAtual];
+    alt3.innerText = altC[questaoAtual];
+    alt4.innerText = altD[questaoAtual];
+    alt5.innerText = altE[questaoAtual];
+
+    questaoAtual += 1;
+}
+
+function resposta(alternativa) {
+
+    escolhidas.push(alternativa); 
+
+    let esconder = document.querySelectorAll(".botao");
+    esconder.disabled = true;
+}
+
+function verAcertos(){
+    for(let i = 0; i < 10; i++){
+        if(escolhidas[i] == respostaCerta[i])
+            acertos++;
+    }
+
+    alert(acertos);
+}
+
+
+
+
